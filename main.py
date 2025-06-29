45ed9d1d1c81dfc6ff584444f57647a34ac64f84
Sanjeet singh <sanjeetsingh07022005@gmail.com>
2025-06-29 08:13:50 +0530

    Update main.py
    added new features

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ eel.init("www")
 playassistantsound()
 
 os.system('start msedge.exe --app="http://localhost:8000/index.html"')
-
+//designig phase
 eel.start('index.html',mode=None,host='localhost', block=True)
